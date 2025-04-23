59a39dba7850f4ae822b55852126bf6cadda2744
Hoyoung Sin <hoyoungsin@gmail.com>
2025-04-23 17:56:52 -0400

    Installed axios for api calls, created folders for pages, components, and services. Created fod component, home page, anura services, and api config

---
+++ src/components/fod.jsx
@@ -0,0 +1,7 @@
+export default function fod ({}) {
+    return (
+        <div>
+            //fod info
+        </div>
+    )
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,19 @@
+import { getFOD } from "../services/anura.js";
+import FOD from "../components/fod.jsx";
+
+
+export default function Home(){
+    //function for frog of the day (fod)    
+
+    return (
+        <>
+            <h1>AnuraFacts</h1>
+            <div className="welcome">
+            Welcome to AnuraFacts, your favotrite fact website for the order of Anura!
+            </div>
+            <div>
+                //fod
+            </div>
+        </>
+    )
+}
